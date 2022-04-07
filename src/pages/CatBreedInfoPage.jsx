e77e6990b81bc6bbc65666216e1d973229755f19
leninner <mazabandalenin180@gmail.com>
2022-04-07 16:56:55 -0500

    Update

--- src/pages/CatBreedInfoPage.jsx
+++ src/pages/CatBreedInfoPage.jsx
@@ -1,8 +1,21 @@
 import { BreedInfoContainer } from '../containers/BreedInfoContainer';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export const CatBreedInfoPage = () => {
+  const navigate = useNavigate();
   const { state } = useLocation();
+
+  useEffect(() => {
+    if (!state) {
+      navigate('/');
+    }
+
+    return () => {
+      console.log('unmount');
+    };
+  }, []);
+
   const { catBreed } = state;
 
   return (
