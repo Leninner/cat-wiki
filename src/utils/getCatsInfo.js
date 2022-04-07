2ecbabd039adf0c95607f740412d22ffcf2c4559
leninner <mazabandalenin180@gmail.com>
2022-04-07 16:42:58 -0500

    Update fetching of data

--- src/utils/getCatsInfo.js
+++ src/utils/getCatsInfo.js
@@ -1,4 +1,8 @@
 export const getCatsInfo = async () => {
+  if (localStorage.getItem('catsInfo')) {
+    return JSON.parse(localStorage.getItem('catsInfo'));
+  }
+
   const response = await fetch(`https://api.thecatapi.com/v1/breeds`, {
     headers: {
       'x-api-key': '274f31b1-d8a7-43dd-855c-cd91492b85b1',
@@ -6,6 +10,7 @@ export const getCatsInfo = async () => {
   });
 
   const data = await response.json();
+  localStorage.setItem('catsInfo', JSON.stringify(data));
 
   return data;
 };
